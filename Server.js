82214d3e9409ed53b9ef91c280a2cf8820bd68ae
shakeerdeveloper-cell <shakeerdeveloper@gmail.com>
2026-08-10 10:12:43 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -5,7 +5,16 @@ const { tavily } = require('@tavily/core');
 
 const app = express();
 app.use(express.json());
-
+// Bypass CORS security blocks
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    next();
+});
 // 1. Initialize Firebase Database
 const serviceAccount = require('./firebase-key.json');
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
